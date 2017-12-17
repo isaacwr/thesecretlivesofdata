2c8f707ac28a4de5a050a5d20cad1b42c6578961
isaacwr <isaac.rothschild@gmail.com>
2017-12-17 13:41:42 -0800

    Reset init

--- paxos/scripts/frames/init.js
+++ paxos/scripts/frames/init.js
@@ -7,9 +7,9 @@ define(["./playground", "./title", "./intro", "./overview", "./election", "./rep
     function (playground, title, intro, overview, election, replication, conclusion) {
         return function (player) {
             //player.frame("playground", "Playground", playground);
-            //player.frame("home", "Home", title);
-            //player.frame("intro", "What is Distributed Consensus?", intro);
-            //player.frame("overview", "Protocol Overview", overview);
+            player.frame("home", "Home", title);
+            player.frame("intro", "What is Distributed Consensus?", intro);
+            player.frame("overview", "Protocol Overview", overview);
             player.frame("election", "Leader Election", election);  // Election is Errors
             player.frame("replication", "Log Replication", replication);  // I think is partition behavior
             player.frame("conclusion", "Other Resources", conclusion);  // Conclusion and  links to paxos made live and paxos simple
